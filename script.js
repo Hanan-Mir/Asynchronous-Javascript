7d0fc1e0f66ba2b9ee608165f0f788ae5ee413b3
Hanan-Mir <Hanan15ite13@gmail.com>
2024-12-05 18:59:36 +0530

    completed the promises and did some challenges

--- script.js
+++ script.js
@@ -156,24 +156,25 @@ let locationPromise=function(){
 //     fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`)
 // });
 
-let whereAmI=function(){
-    locationPromise().then((res)=>{
-        let {latitude:lat,longitude:lng}=res.coords;
-        return fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`);
-    }).then((res)=>{
-        if(!res.ok) throw new Error("country not found")
-            return res.json();
-    }).then(data=>{
-let countryCode=data.countryCode;
-return fetch(`https://restcountries.com/v2/alpha/${countryCode}`)
-    }).then((res)=>res.json()).then((data)=>renderCountry(data)).catch(err=>console.error(err));
-}
-btn.addEventListener('click',whereAmI);
+// let whereAmI=function(){
+//     locationPromise().then((res)=>{
+//         let {latitude:lat,longitude:lng}=res.coords;
+//         return fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`);
+//     }).then((res)=>{
+//         if(!res.ok) throw new Error("country not found")
+//             return res.json();
+//     }).then(data=>{
+// let countryCode=data.countryCode;
+// return fetch(`https://restcountries.com/v2/alpha/${countryCode}`)
+//     }).then((res)=>res.json()).then((data)=>renderCountry(data)).catch(err=>console.error(err));
+// }
+// btn.addEventListener('click',whereAmI);
 //-------------------------------------coding challenge 2--------------------------------------------------
 let imageContainer=document.querySelector('.images');
+let img;
 let createImage=function(imgPath){
 return new Promise(function(resolve,reject){
-   let img= document.createElement('img');
+   img= document.createElement('img');
     img.src=imgPath;
     img.addEventListener('load',function(){
         imageContainer.append(img)
@@ -186,10 +187,139 @@ img.addEventListener('error',function(){
 })
 })
 }
-createImage('img/img-1.jpg').then(img=>console.log('image 1 loaded')).catch(err=>console.error(err));
+// let currentImage;
+// createImage('img/img-1.jpg').then(img=>{console.log('image 1 loaded')
+//     currentImage=img;
+//     return timeoutPromise(5);
+// }).then(res=>{
+//     console.log('Execution stoped for 2 seconds')
+// currentImage.style.display='none';
+// return createImage('img/img-2.jpg');
+// }).then(res=>{
+//     currentImage=res;
+//     console.log('Image 2 loaded');
+//     return timeoutPromise(5)
+// }).then(()=>currentImage.style.display='none');
+//------------------------------async and await------------------------------------------------------
+let whereAmI=async function(country){
+//getting country using geolocation api
+try{
+let response=await locationPromise();
+let {latitude:lat,longitude:lng}=response.coords;
+console.log(lat,lng);
+let getLocation=await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`);
+console.log(getLocation);
+if(!getLocation.ok) throw new Error(`Something wrong in the url of fetch`);
+let locationData=await getLocation.json();
+let countryCode=locationData.countryCode;
+let getCountryResponse=await fetch(`https://restcountries.com/v2/alpha/${countryCode}`);
+let data=await getCountryResponse.json();
+renderCountry(data);
 
+    //getting country using async and await
+    // let res=await fetch(`https://restcountries.com/v2/alpha/${country}`)
+    // let data=await res.json();
+    // renderCountry(data);
+    // console.log(data);
+return `You are in ${locationData.city},${data.name}`;
+} catch(err){
+renderError(err.message);
+//throwin an error explicitly
+throw new Error(err.message);
+}
+}
+btn.addEventListener('click',whereAmI);
 
+//----------------------------using try catch--------------------------------
+// try{
+//     let x=9;
+// const z=100;
+// z=90;
+// console.log(z);
+// }catch(err){
+//     alert(err);
+// }
+// whereAmI().then(str=>console.log(str));
+console.log(`1:loading the data`);
+// whereAmI().then(str=>console.log(str)).catch((err)=>console.log(`2:${err.message}`)).finally(console.log(`3:data loaded sucessfully`));
+// console.log(`3:data loaded sucessfully`);
 
-
-
-
+(async function(){
+    try{
+let outputStr=await whereAmI();
+console.log(`2:${outputStr}`)
+    }
+    catch(err){
+console.log(`3:${err.message}`)
+    }
+})();
+//----------------------running promises in parallel------------------------------------
+let getCapital=async function(c1,c2,c3){
+    // let res1=await fetch(`https://restcountries.com/v2/name/${c1}`);
+    // console.log(res1);
+    // let [data1]=await res1.json();
+    
+    // let res2=await fetch(`https://restcountries.com/v2/name/${c2}`);
+    // let [data2]=await res2.json();
+    // let res3=await fetch(`https://restcountries.com/v2/name/${c3}`);
+    // let [data3]=await res3.json();
+    // let capitalArray=[data1.capital,data2.capital,data3.capital]
+    // console.log(capitalArray);
+    //Running all promises in parallel
+    try{
+   const data=await Promise.all([fetchJson(`https://restcountries.com/v2/name/${c1}`),fetchJson(`https://restcountries.com/v2/name/${c2}`),fetchJson(`https://restcountries.com/v2/name/${c3}`)]);
+console.log(data.map(d=>d[0].capital));
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+getCapital("portugal","Germany","usa");
+(async function(){
+    const promiseWait=await Promise.race([fetchJson(`https://restcountries.com/v2/name/egypt`),fetchJson(`https://restcountries.com/v2/name/bangladesh`),fetchJson(`https://restcountries.com/v2/name/pakistan`)]);
+  console.log(promiseWait[0]);
+})();
+// let tickPromise=function(sec){
+//     return new Promise(function(_,reject){
+//         setTimeout(reject(new Error(`timeout baby`))},sec*1000)
+//     });
+// }
+let tickPromise=function(sec){
+return new Promise(function(_,reject){
+    setTimeout(reject(new Error('Promise rejected baby')),sec*100000)
+})
+}
+Promise.race([fetchJson(`https://restcountries.com/v2/name/italy`),tickPromise(10)]).then(res=>console.log(res)).catch(err=>console.error(err.message));
+Promise.allSettled([
+    Promise.resolve("resolved"),
+    Promise.reject('rejected promise'),
+    Promise.resolve('another rejection')
+]).then(res=>console.log(res));
+//----coding challenge #3---------
+// let loadNPause=async function(){
+//     try{
+// let img1=await createImage(`img-1.jpg`);
+// console.log(`image 1 loaded`);
+// await timeoutPromise(2);
+// img1.style.display='none'
+// let img2=await createImage(`img-2.jpg`);
+// console.log(`image 2 loaded`);
+// await timeoutPromise(2);
+// img2.style.display='none';
+//     }
+//     catch(err){
+// console.error(err.message);
+//     }
+// }
+//loadNPause();
+let loadAll=async function(imgArr){
+    try{
+imgArr.map(async el=>{await createImage(el);
+console.log(el)
+})
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+loadAll(['img-1.jpg','img-2.jpg','img-3.jpg']);
